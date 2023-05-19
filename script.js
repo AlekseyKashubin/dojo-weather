0ce57569f5af8433ab763c7f01f2511f280536ce
AlekseyKashubin <Akashubin@yahoo.com>
2023-05-19 11:04:07 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function message(element) {
+    return "Loading weather report...";
+}
+
+
+function removeMe(element) {
+    document.getElementById("cookies").style.display = "none";
+}
+
+
+
+
+
+
+
+
+function chooseOne(element) {
+    document.getElementById("c1").innerText = "75" + String.fromCharCode(176);
+    document.getElementById("c2").innerText = "80" + String.fromCharCode(176);
+    document.getElementById("c3").innerText = "69" + String.fromCharCode(176);
+    document.getElementById("c4").innerText = "78" + String.fromCharCode(176);
+    document.getElementById("f1").innerText = "65" + String.fromCharCode(176);
+    document.getElementById("f2").innerText = "66" + String.fromCharCode(176);
+    document.getElementById("f3").innerText = "61" + String.fromCharCode(176);
+    document.getElementById("f4").innerText = "70" + String.fromCharCode(176);
+
+
+}
